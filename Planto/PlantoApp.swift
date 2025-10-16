6c56be4b9dbb2bdcbcd7936eb4d6f8c5f177cabe
DanaNB02 <dbaeisa25@twq.idserve.net>
2025-10-16 19:16:14 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Dana on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
